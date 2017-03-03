cdeb0c291659cd40148a4b75288254e7e6b21f8c
Donnie Ellstrom <dsvene@gmail.com>
2017-03-02 21:39:18 -0800

    Edit credits

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use('/movie', require('./controllers/movies'));
 
-mongoose.connect('mongodb://localhost/subtitles');
+// mongoose.connect('mongodb://localhost/subtitles');
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'static/index.html'));
